77d21fe0c4469e1fd9373a476550f7fee5fc0101
beltegeuse <adrien.gruson@gmail.com>
2011-10-05 10:55:08 +0200

    Ajout d'un exemple simple Cuda sans OpenGL.

--- Tests/CudaC3/main.cpp
+++ Tests/CudaC3/main.cpp
@@ -1,6 +1,11 @@
 #include <cutil_inline.h>
 #include <iostream>
 
+// Pour lancer le kernel
+extern "C" 
+void runKernel(int * a, int * b, int * res, int size);
+
+
 int main()
 {
 	int width = 512;
@@ -9,20 +14,60 @@ int main()
 	// Creation du device
 	cutilSafeCall( cudaSetDevice( cutGetMaxGflopsDeviceId() ) );
 
-	// Allocation des objects
-	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
-    cudaArray* cu_array;
-    cutilSafeCall( cudaMallocArray( &cu_array, &channelDesc, width, height )); 
+	// Creation des buffers sur CPU
+	int * a = new int[width*height];
+	int * b = new int[width*height];
+	int * res = new int[width*height];
+	for(int i = 0; i < width*height; i++)
+	{
+		a[i] = (int)ceil(((double)rand()/ (double)RAND_MAX)*100);
+		b[i] = (int)ceil(((double)rand()/ (double)RAND_MAX)*100);
+	}
 
-	// Gestion de la taille de la grille
-	dim3 dimBlock(8, 8, 1);
-    dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
+	// Allocation des objects on the device
+	// *** data
+	unsigned int size = width * height * sizeof(int);
+	std::cout << "Allocation d'un buffer de taille " << width*height << "\n";
+	int* d_a = NULL;
+	int* d_b = NULL;
+	int* d_res = NULL;
+    cutilSafeCall( cudaMalloc( (void**) &d_a, size));
+	cutilSafeCall( cudaMalloc( (void**) &d_b, size));
+	cutilSafeCall( cudaMalloc( (void**) &d_res, size));
 
-    // warmup
-    //transformKernel<<< dimGrid, dimBlock, 0 >>>( d_data, width, height, angle);
+	// Copy des donnees
+	cutilSafeCall( cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice));
+	cutilSafeCall( cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice));
+
+    // Lancer le calcul
+	std::cout << "Lancer le kernel ... \n";
+	runKernel(d_a, d_b, d_res, width*height);
+	cutilSafeCall( cutilDeviceSynchronize() );
+
+	// Copie DeviceHost
+	cutilSafeCall( cudaMemcpy(res, d_res, size, cudaMemcpyDeviceToHost));
+
+	// Verification du test
+	int i = 0;
+	for(;i < width*height;i++)
+		if(res[i] != a[i] + b[i])
+			std::cout << "Error : [" << i << "] " << res[i] << " != " << a[i] << " + " << b[i] << std::endl;
+
+
+	// Liberation des ressources
+	// *** Device
+	cutilSafeCall(cudaFree(d_a));
+	cutilSafeCall(cudaFree(d_b));
+	cutilSafeCall(cudaFree(d_res));
+	// *** CPU
+	delete[] res;
+	delete[] a;
+	delete[] b;
 
 	// Close device
 	cutilDeviceReset();
+	std::cout << "Test result : " << ((i == width*height) ? "Succes" : "Error" ) <<  std::endl;
+	std::cout.flush();
 
 	return 0;
 }
--- Tests/CudaC3/test_kernel.cu
+++ Tests/CudaC3/test_kernel.cu
@@ -1,11 +1,18 @@
 
 #include <cuda.h>
 
-inline __global__ void KernelTest(float * data, int width, int height)
+inline __global__ void KernelTest(int * a, int * b, int * res, int size)
 {
-	// calculate normalized texture coordinates
+	// Calcul de l'indice du tableau
     unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+	res[x] = a[x] + b[x];
+}
 
-	return;
+extern "C"
+void runKernel(int * a, int * b, int * res, int size)
+{
+	dim3 dimBlock(64, 1, 1);
+    dim3 dimGrid(size / dimBlock.x, 1, 1);
+    
+	KernelTest<<< dimGrid, dimBlock, 0 >>>( a, b, res, size);
 }
